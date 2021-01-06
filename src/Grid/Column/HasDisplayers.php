c5152c858bbac7b6fca205a1521c6c8757e2bb70
vagra <vagra@sina.com>
2021-01-06 09:28:30 +0800

    为 Grid\Field 和 Show\Field 添加 nl2br() 方法。

--- src/Grid/Column/HasDisplayers.php
+++ src/Grid/Column/HasDisplayers.php
@@ -77,6 +77,18 @@ trait HasDisplayers
     }
 
     /**
+     * Convert '\n' to <br />
+     *
+     * @return $this
+     */
+    public function nl2br()
+    {
+        return $this->display(function ($value) {
+            return nl2br($value);
+        });
+    }
+
+    /**
      * Display column with "long2ip".
      *
      * @param null $default
